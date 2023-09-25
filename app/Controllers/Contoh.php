43ce2ea017e40fb97c375d69083c0260054fe634
hendrawan-19220059 <19220059@bsi.ac.id>
2023-09-25 19:35:47 +0700

    menambahkan latihan-1 dan routingannya

--- app/Controllers/Contoh.php
+++ app/Controllers/Contoh.php
@@ -6,6 +6,8 @@ class Contoh extends BaseController
 {
     public function index()
     {
-        echo "<h1>Test</h1>";
+        // echo "<h1>Test</h1>";
+        return view('latihan-1.php');
+
     }
 }
